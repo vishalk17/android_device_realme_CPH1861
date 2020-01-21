d05833e7562a02ee0703b5857e3401c5cf0ed058
vishalk17 <vishalkapadi95@gmail.com>
2020-01-21 17:41:49 +0000

    props.mk: Add additional properties

--- props.mk
+++ props.mk
@@ -1,2 +1,8 @@
 # Adb= ?
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.usb.config=adb
+
+# Misc
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera.disable_zsl_mode=1 \
+    ro.oem_unlock_supported=1 \
+    ro.vendor.rc=/vendor/etc/init/hw/
